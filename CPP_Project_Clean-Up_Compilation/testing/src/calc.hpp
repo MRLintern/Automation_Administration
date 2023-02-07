71b16ddcc4edc54dfd3fef5a05ec11f2b24a1bf8
Matthew Lintern-Monro <86484124+MRLintern@users.noreply.github.com>
2023-02-07 19:21:44 +0000

    Initial Commit

---
+++ CPP_Project_Clean-Up_Compilation/testing/src/calc.hpp
@@ -0,0 +1,17 @@
+#ifndef __CALC__
+#define __CALC__
+
+//--function prototypes--//
+
+//addition
+double add(double, double);
+//subtraction
+double subtract(double, double);
+//multiplication
+double multiply(double, double);
+//division
+double division(double, double);
+
+
+
+#endif
